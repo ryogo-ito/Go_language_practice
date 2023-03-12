8ab7231636ffca9bd3c60680ea13a66035a96d03
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-03-13 01:18:25 +0900

    構造体のpractice

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "Go_language_practice/section3"
+import (
+	"fmt"
+
+	"Go_language_practice/section3"
+)
 
 func main() {
-	section3.New()
+	v := section3.Vertex{1, 2, "test"}
+
+	fmt.Println("関数実行前", v)
+	section3.ChangeVertex(v)
+	fmt.Println("関数実行後", v)
+
+	v2 := &section3.Vertex{1, 2, "test"}
+	section3.ChangeVertex2(v2)
+	fmt.Println(v2)
 }
