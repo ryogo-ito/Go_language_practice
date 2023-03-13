4e0b7560bf0624170b64ea11f844522fb6e3f80a
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-03-14 01:21:02 +0900

    アサーションとswitchtype文のpractice

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"Go_language_practice/section4"
-)
+import "Go_language_practice/section4"
 
 func main() {
-	var mike section4.Human = &section4.Person{"Mike"}
-	section4.DriveCar(mike)
-	fmt.Println(mike)
+	section4.Do(10)
+	section4.Do("interface")
+	section4.Do(true)
 }
---
+++ section4/assertion.go
@@ -0,0 +1,14 @@
+package section4
+
+import "fmt"
+
+func Do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Printf("I don't know %T", v)
+	}
+}
