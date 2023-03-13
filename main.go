f301fba875a9454b83295382508153c4ae3064ef
ryogo-ito <106786725+ryogo-ito@users.noreply.github.com>
2023-03-14 01:37:59 +0900

    stringerのpractice

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "Go_language_practice/section4"
+import (
+	"fmt"
+
+	"Go_language_practice/section4"
+)
 
 func main() {
-	section4.Do(10)
-	section4.Do("interface")
-	section4.Do(true)
+	mike := section4.Person2{"Mike", 26}
+	fmt.Println(mike)
 }
